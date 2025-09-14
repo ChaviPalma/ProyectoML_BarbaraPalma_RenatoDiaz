865f7ad4592754fca96c449f348f3ca3d8c8345c
Nato9-7 <re.diazs@duocuc.cl>
2025-09-14 15:25:17 -0300

    Cambios en nodes de data_processing para salida de solo 1 parquet

--- proyectoml-renatodiaz-barbarapalma/src/proyecto_ml/pipelines/data_processing/nodes.py
+++ proyectoml-renatodiaz-barbarapalma/src/proyecto_ml/pipelines/data_processing/nodes.py
@@ -13,16 +13,10 @@ def limpiar_anime_dataset(anime_dataset: pd.DataFrame) -> pd.DataFrame:
     
     return anime_dataset
 
-import pandas as pd
-
 def limpiar_convertir_episodes(anime_dataset: pd.DataFrame) -> pd.DataFrame:
     # Reemplazar 'UNKNOWN' por 0
     anime_dataset['Episodes'] = anime_dataset['Episodes'].replace('UNKNOWN', 0)
     # Convertir a numérico, coerción de errores, rellenar NaN con 0 antes de convertir a int
     anime_dataset['Episodes'] = pd.to_numeric(anime_dataset['Episodes'], errors='coerce').fillna(0).astype(int)
-    return anime_dataset
-
-def convertir_premiered_booleano(anime_dataset: pd.DataFrame) -> pd.DataFrame:
-    # Reemplazar 'UNKNOWN' por False, otros por True
-    anime_dataset.loc[:, 'Premiered'] = anime_dataset['Premiered'] != 'UNKNOWN'
+    
     return anime_dataset
